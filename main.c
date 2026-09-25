bfc8db0b2a11bc146218cf05b60b334e4f7a70c8
Kate Macias <kebohning@gmail.com>
2026-09-24 17:06:11 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+/**************************************** 
+ * C-ploration 0 for CS 271 
+ *   
+ * [NAME] $Kate Macias$  
+ * [TERM] FALL $2026$  
+ *****************************************/
+#include <stdio.h>
+int main(void) {
+   // printf(.) displays the string given
+   printf("Hello world!\n");
+   // main returns 0 if exiting without error
+   return 0;
+}
